6d98104a1c553d9577acef4daf4d68cbed44d898
Manabu Hirano <hirano@toyota-ct.acjp>
2024-08-19 17:19:06 +0900

    first commit

---
+++ etc/README.md
@@ -0,0 +1,1 @@
+These files were used in creating dataset in mix/i3-gen12/ddr4-2133-16g/{Office_REvil, Firefox_Office_REvil}/.
---
+++ list-contents.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/bash
+
+DATA_REPO_DIR_MEMORY=.
+
+checkNumberOfCsvFile() {
+  dir_path=$1
+  if [ ! -d ${dir_path} ]; then
+	return
+  fi
+  echo "NUMBER OF CSV FILES:" `echo $1` 
+  dirs=`find $dir_path -mindepth 1 -maxdepth 1 -type d | sort`
+  for dir in $dirs;
+  do
+     echo -en "\t " `find $dir -name "ata_write.*" | wc -l` 
+     echo -en "\t (CSV\t " `find $dir -name "ata_write.csv" | wc -l` 
+     echo -e "\t ZIP\t " `find $dir -name "ata_write.zip" | wc -l` ")\t" `basename $dir` 
+  done
+}
+
+memory_types_celeron=("ddr4-2133-16g" "ddr4-2133-8g" "ddr4-1866-16g" "ddr4-1866-8g" "ddr4-1600-16g" "ddr4-1600-8g")
+memory_types_i3=("ddr4-2666-16g" "ddr4-2666-8g" "ddr4-3200-16g" "ddr4-3200-8g" "ddr4-2133-16g" "ddr4-2133-8g")
+
+echo "============"
+echo "  ORIGINAL  "
+echo "============"
+echo "========"
+echo "Celeron"
+echo "========"
+for (( i=0; i < ${#memory_types_celeron[@]}; i++)) {
+  checkNumberOfCsvFile "${DATA_REPO_DIR_MEMORY}/dataset/original/celeron-gen6/${memory_types_celeron[$i]}"
+}
+echo "============"
+echo "i3"
+echo "============"
+for (( i=0; i < ${#memory_types_i3[@]}; i++)) {
+  checkNumberOfCsvFile "${DATA_REPO_DIR_MEMORY}/dataset/original/i3-gen12/${memory_types_i3[$i]}"
+}
+echo "==================="
+echo "TOTAL OF ORIGINAL:"
+echo "==================="
+checkNumberOfCsvFile "${DATA_REPO_DIR_MEMORY}/dataset/original/"
+
+echo "==========="
+echo "EXTRA "
+echo "==========="
+types=("i3-gen12/ddr5-4800-16g" "i5-gen12/ddr4-2133-16g" "i7-gen12/ddr4-2133-16g")
+for (( i=0; i < ${#types[@]}; i++)) {
+  checkNumberOfCsvFile "${DATA_REPO_DIR_MEMORY}/dataset/extra/${types[$i]}"
+}
+echo "==================="
+echo "TOTAL OF EXTRA:"
+echo "==================="
+checkNumberOfCsvFile "${DATA_REPO_DIR_MEMORY}/dataset/extra/"
+
+echo "==========="
+echo "MIX "
+echo "==========="
+types=("i3-gen12/ddr4-2133-16g")
+for (( i=0; i < ${#types[@]}; i++)) {
+  checkNumberOfCsvFile "${DATA_REPO_DIR_MEMORY}/dataset/mix/${types[$i]}"
+}
+echo "==================="
+echo "TOTAL OF MIX:"
+echo "==================="
+checkNumberOfCsvFile "${DATA_REPO_DIR_MEMORY}/dataset/mix/"
+
+echo "==========="
+echo "VARIANTS"
+echo "==========="
+types=("i3-gen12/ddr4-2133-16g")
+for (( i=0; i < ${#types[@]}; i++)) {
+  checkNumberOfCsvFile "${DATA_REPO_DIR_MEMORY}/dataset/variants/${types[$i]}"
+}
+echo "==================="
+echo "TOTAL OF VARIANTS:"
+echo "==================="
+checkNumberOfCsvFile "${DATA_REPO_DIR_MEMORY}/dataset/variants/"
+
+echo "==================="
+echo "TOTAL:"
+echo "==================="
+checkNumberOfCsvFile "${DATA_REPO_DIR_MEMORY}/dataset/"
+
